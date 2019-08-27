a153dcaef934a4a782c81adc4d4d52194c726928
acukovic <aleksacukovic1@gmail.com>
2019-08-27 21:16:37 +0200

    Implement shuffle_arrays test

--- tests/utils/test_data_utils.py
+++ tests/utils/test_data_utils.py
@@ -4,6 +4,7 @@ from numpy.testing import assert_allclose
 from nnlib.utils.data_utils import (
     one_hot,
     pad_with_zeros,
+    shuffle_arrays,
     train_test_split
 )
 
@@ -12,12 +13,33 @@ def test_one_hot():
     pass
 
 
+def test_shuffle_arrays():
+    x = np.arange(15)
+    y = np.arange(15)
+    z = np.arange(15)
+
+    shuffle_arrays(x, y)
+
+    assert (x == y).all()
+    assert not (x == z).all()
+
+    np.random.seed(0)
+    np.random.shuffle(x)
+
+    shuffle_arrays(y, seed=0)
+
+    assert (x == y).all()
+
+
 def test_pad_with_zeros():
     array = np.arange(3*3*4*4).reshape(3, 3, 4, 4)
     padding = {2: 2, 3: 2}
+
     expected = np.zeros(shape=(3, 3, 6, 6), dtype=array.dtype)
     expected[:, :, 1:5, 1:5] = array
+
     got = pad_with_zeros(array, padding)
+
     assert (expected == got).all()
 
 
